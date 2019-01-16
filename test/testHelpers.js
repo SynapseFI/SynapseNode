df32147e24dc09fcacde87a7bd2fc3ecc07ff127
Easak Hong <easakhong@Easaks-MacBook-Pro.local>
2019-01-16 12:23:12 -0800

    Update readme and test helpers

--- test/testHelpers.js
+++ test/testHelpers.js
@@ -1,10 +1,13 @@
 const Client = require('../src/lib/Client');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const Helpers = {
   client: new Client({
-    client_id: '',
-    client_secret: '',
-    fingerprint: '',
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    fingerprint: process.env.FINGERPRINT,
     ip_address: '127.0.0.1',
     isProduction: false
   })
